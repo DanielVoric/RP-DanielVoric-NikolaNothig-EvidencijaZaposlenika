27bcb996ec5a8bc6dbdb0d9a2b41d292709cfab3
DanielVoric <voricdaniel@gmail.com>
2023-02-16 20:42:00 +0100

    Kreiranje tablice zaposlenika

--- springboot-backend/src/main/java/project/voricnothig/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/project/voricnothig/springboot/SpringbootBackendApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
-
-	public static void main(String[] args) {
+// cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+// mysql -u rpuser -p
+// 1234
+// Za pokretanje servera u konzoli
+ 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
---
+++ springboot-backend/src/main/java/project/voricnothig/springboot/modul/Zaposlenici.java
@@ -0,0 +1,30 @@
+package project.voricnothig.springboot.modul;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "zaposlenici")
+public class Zaposlenici {
+// Stvaranje tablice zaposlenici
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "ime")
+    private String ime;
+
+    @Column(name = "prezime")
+    private String prezime;
+
+    @Column(name = "email_id")
+    private String emailid;
+}
