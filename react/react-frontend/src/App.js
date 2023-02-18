f8ad11a489dabcdcbb4431e5dae538d7595ebfd1
DanielVoric <voricdaniel@gmail.com>
2023-02-18 16:59:54 +0100

    stvaranje ruta

--- react/react-frontend/src/App.js
+++ react/react-frontend/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import ListaZaposlenikaC from './komponente/ListaZaposlenikaC';
 import PodnozjeC from './komponente/PodnozjeC';
 import ZaglavljeC from './komponente/ZaglavljeC';
 
 function App() {
   return (
-    <div >
-      <ZaglavljeC/>
-      <ListaZaposlenikaC />
-      <PodnozjeC />
+    <div>
+      <BrowserRouter>
+        <ZaglavljeC />
+        <Routes>
+          <Route index element={<ListaZaposlenikaC />} />
+          <Route path="/" element={<ListaZaposlenikaC />}></Route>
+          <Route path="/zaposlenici" element={<ListaZaposlenikaC />}></Route>
+          <Route path="*" element={<ListaZaposlenikaC />}></Route>
+        </Routes>
+        <PodnozjeC />
+      </BrowserRouter>
     </div>
   );
 }
+
+
+
+
 
 export default App;
--- react/react-frontend/src/Komponente/PodnozjeC.js
+++ react/react-frontend/src/Komponente/PodnozjeC.js
@@ -3,11 +3,11 @@ import React from 'react'
 const PodnozjeC = () => {
   return (
     <div>
-        <footer className='footer'>
-            <span classname="text-muted">
-                Izradili Daniel Vorić i Nikola Nöthig
-            </span>
-        </footer>
+      <footer className="footer">
+        <span className="text-muted">
+          Izradili Daniel Vorić i Nikola Nöthig
+        </span>
+      </footer>
     </div>
   )
 }
