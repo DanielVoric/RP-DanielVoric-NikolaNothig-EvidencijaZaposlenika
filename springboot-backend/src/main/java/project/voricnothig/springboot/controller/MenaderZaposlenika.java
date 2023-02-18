63ac593f7322893b9d29772b26f2df0d2ad47439
Nikola <nothignikola@email.com>
2023-02-18 17:46:04 +0100

    stvoren kreirajZaposlenika rest api

--- springboot-backend/src/main/java/project/voricnothig/springboot/controller/MenaderZaposlenika.java
+++ springboot-backend/src/main/java/project/voricnothig/springboot/controller/MenaderZaposlenika.java
@@ -2,10 +2,7 @@ package project.voricnothig.springboot.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.voricnothig.springboot.modul.Zaposlenik;
 import project.voricnothig.springboot.repository.TeglicaZaposlenika;
 
@@ -22,5 +19,11 @@ public class MenaderZaposlenika {
     @GetMapping
     public List<Zaposlenik> dohvatiSveZaposlenike(){
         return teglicaZaposlenika.findAll();
+    }
+
+    //kreirajZaposlenika rest api
+    @PostMapping
+    public Zaposlenik kreirajZaposlenika(@RequestBody Zaposlenik zaposlenik){
+        return teglicaZaposlenika.save(zaposlenik);
     }
 }
