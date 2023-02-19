2ef37040481728c53085b3dd6f7865ba93ce8da3
DanielVoric <voricdaniel@gmail.com>
2023-02-19 22:43:15 +0100

    stvoren api za dohvacanje zaposlenika po id-u

--- springboot-backend/src/main/java/project/voricnothig/springboot/controller/MenaderZaposlenika.java
+++ springboot-backend/src/main/java/project/voricnothig/springboot/controller/MenaderZaposlenika.java
@@ -2,7 +2,9 @@ package project.voricnothig.springboot.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project.voricnothig.springboot.exception.NijePronadenoException;
 import project.voricnothig.springboot.modul.Zaposlenik;
 import project.voricnothig.springboot.repository.TeglicaZaposlenika;
 
@@ -26,4 +28,13 @@ public class MenaderZaposlenika {
     public Zaposlenik kreirajZaposlenika(@RequestBody Zaposlenik zaposlenik){
         return teglicaZaposlenika.save(zaposlenik);
     }
-}
+
+    //dohvati zaposlenika po id-u Rest api
+    @GetMapping("{id}")
+    public ResponseEntity<Zaposlenik> dohvatiZaposlenikaPoId(@PathVariable long id){
+        Zaposlenik zaposlenik = teglicaZaposlenika.findById(id)
+                .orElseThrow(() -> new NijePronadenoException("Zaposlenik ne postoji sa tim id-om" + id));
+        return ResponseEntity.ok(zaposlenik);
+
+    }    }
+
